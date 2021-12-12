168da32d881e1f26f460cf32b72bc00d1ef28d2a
joelgrod <joelgrod@gmail.com>
2021-12-12 09:18:26 +0000

    About page

--- src/domains/portfolio/pages/About.vue
+++ src/domains/portfolio/pages/About.vue
@@ -9,18 +9,31 @@
               <ion-list-header>
                 <ion-label>
                   <ion-text color="dark">
-                    <h2 class="ion-text-uppercase">Experience</h2>
+                    <h2 class="ion-text-uppercase">
+                      <strong>Experience</strong>
+                    </h2>
                   </ion-text>
                 </ion-label>
               </ion-list-header>
               <ion-item-group v-for="(exp, index) in experiences" :key="index">
                 <ion-item-divider>
-                  <ion-label>
+                  <ion-label slot="start" class="ion-text-start">
                     <ion-text color="secondary">
-                      <h3>{{ exp.profile }} - {{ exp.company }}</h3>
+                      <h3><strong>{{ exp.profile }}</strong></h3>
                     </ion-text>
                     <ion-text color="primary">
-                      <h4>{{ exp.from }} - {{ exp.to }}</h4>
+                      <h4 class="pt-02">{{ exp.company }}</h4>
+                    </ion-text>
+                    <ion-text color="medium" class="ion-hide-sm-up">
+                      <h5>{{ exp.from }} - {{ exp.to }}</h5>
+                    </ion-text>
+                  </ion-label>
+                  <ion-label slot="end" class="ion-padding-end ion-text-end ion-hide-sm-down">
+                    <ion-text color="medium">
+                      <p>{{ exp.location }}</p>
+                    </ion-text>
+                    <ion-text color="medium" class="ion-float-end">
+                      <h5>{{ exp.from }} - {{ exp.to }}</h5>
                     </ion-text>
                   </ion-label>
                 </ion-item-divider>
@@ -41,34 +54,40 @@
               <ion-list-header>
                 <ion-label>
                   <ion-text color="dark">
-                    <h2 class="ion-text-uppercase">Education</h2>
+                    <h2 class="ion-text-uppercase">
+                      <strong> Education </strong>
+                    </h2>
                   </ion-text>
                 </ion-label>
               </ion-list-header>
               <ion-item-group v-for="(edu, index) in educations" :key="index">
                 <ion-item-divider>
-                  <ion-label>
+                  <ion-label slot="start" class="ion-text-start">
                     <ion-text color="secondary">
-                      <h3>{{ edu.title }}</h3>
+                      <h3><strong>{{ edu.title }}</strong></h3>
                     </ion-text>
                     <ion-text color="primary">
+                      <h4 class="pt-02">{{ edu.academy }}</h4>
+                    </ion-text>
+                    <ion-text color="medium" class="ion-hide-sm-up">
+                      <h5>{{ edu.from }} - {{ edu.to }}</h5>
+                    </ion-text>
+                  </ion-label>
+                  <ion-label slot="end" class="ion-padding-end ion-text-end ion-hide-sm-down">
+                    <ion-text color="medium">
+                      <p>{{ edu.location }}</p>
+                    </ion-text>
+                    <ion-text color="medium" class="ion-float-end">
                       <h4>{{ edu.from }} - {{ edu.to }}</h4>
                     </ion-text>
                   </ion-label>
                 </ion-item-divider>
-                <ion-item lines="none">
-                  <ion-label>
-                    <p>{{ edu.academy }}</p>
-                  </ion-label>
-                  <ion-text color="dark">
-                      <p>{{ edu.location }}</p>
-                  </ion-text>
-                </ion-item>
                 <ion-item>
                   <ion-label>
                     <ion-text color="dark">
                       <p class="text-content">{{ edu.desc }}</p>
-                    </ion-text></ion-label>
+                    </ion-text></ion-label
+                  >
                 </ion-item>
               </ion-item-group>
             </ion-list>
@@ -79,28 +98,33 @@
             <ion-list>
               <ion-list-header>
                 <ion-label>
-                  <ion-text color="primary">
-                    <h2 class="ion-text-uppercase">Lang</h2>
+                  <ion-text color="dark">
+                    <h2 class="ion-text-uppercase">
+                      <strong> Lang </strong>
+                    </h2>
                   </ion-text>
                 </ion-label>
               </ion-list-header>
-              <ion-item-group v-for="lang, index in langs" :key="index">
-                <ion-item-divider>
-                  <ion-label>
+              <ion-item-group v-for="(lang, index) in langs" :key="index">
+                <ion-item lines="none">
+                  <ion-label slot="start" class="ion-text-start">
                     <ion-text color="secondary">
-                      <h3>{{lang.lang}}</h3>
+                      <h3>
+                        <strong>{{ lang.lang }}</strong>
+                      </h3>
+                    </ion-text>
+                    <ion-text color="primary" class="ion-float-start">
+                      <h4>{{ lang.level }}</h4>
                     </ion-text>
                   </ion-label>
-                </ion-item-divider>
-                <ion-item lines="none">
-                  <ion-label>{{lang.level}}</ion-label>
-                  <ion-label>
-                    <ion-icon 
-                    v-for="star, index in lang.stars" 
-                    :key="index" 
-                    slot="end"
-                    color="success"
-                    :icon="lang.icon"></ion-icon>
+                  <ion-label slot="end" class="ion-text-end">
+                    <ion-icon
+                      v-for="(star, index) in lang.stars"
+                      :key="index"
+                      slot="end"
+                      color="warning"
+                      :icon="lang.icon"
+                    ></ion-icon>
                   </ion-label>
                 </ion-item>
               </ion-item-group>
@@ -112,24 +136,29 @@
             <ion-list>
               <ion-list-header>
                 <ion-label>
-                  <ion-text color="primary">
-                    <h2 class="ion-text-uppercase">Knowledge</h2>
+                  <ion-text color="dark">
+                    <h2 class="ion-text-uppercase">
+                      <strong> Knowledge </strong>
+                    </h2>
                   </ion-text>
                 </ion-label>
               </ion-list-header>
               <ion-grid fixed>
                 <ion-row>
-                  <ion-col size="auto" v-for="knows, index in knowledges" :key="index">
+                  <ion-col
+                    size="auto"
+                    v-for="(knows, index) in knowledges"
+                    :key="index"
+                  >
                     <ion-item>
-                      <ion-icon slot="start" :icon="knows.icon"></ion-icon> 
-                      <ion-label>{{knows.name}}</ion-label>
+                      <ion-icon slot="start" :icon="knows.icon"></ion-icon>
+                      <ion-label>{{ knows.name }}</ion-label>
                     </ion-item>
                   </ion-col>
                 </ion-row>
               </ion-grid>
             </ion-list>
           </ion-col>
-
         </ion-row>
       </ion-grid>
     </ion-content>
@@ -151,7 +180,8 @@ import {
   IonItemDivider,
   IonItem,
   IonLabel,
-  IonIcon
+  IonIcon,
+  IonText,
 } from "@ionic/vue";
 import useData from "../composables/useData";
 
@@ -170,7 +200,8 @@ export default defineComponent({
     IonItemDivider,
     IonItem,
     IonLabel,
-    IonIcon
+    IonIcon,
+    IonText,
   },
   setup() {
     const {
@@ -199,6 +230,9 @@ export default defineComponent({
 }
 .purple {
   background-color: purple;
+}
+.pt-02 {
+  padding-top: 0.2rem;
 }
 .text-content {
   white-space: pre-line;
